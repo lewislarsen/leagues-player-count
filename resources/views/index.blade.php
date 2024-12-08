06dfa75504cc055b2771a7d25266b39bde92bb52
Lewis Larsen <lewis@larsens.dev>
2024-12-08 09:45:25 +0000

    added favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Leagues Player Activity</title>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
 </head>
 <body class="bg-gray-800 h-full flex justify-center items-center">
 <div class="max-w-3xl mt-20 mx-auto px-6 py-8 bg-gray-900 rounded-lg shadow-lg">
